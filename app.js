85b848e56e84ea5845d8f1379f6b73987f98e63a
Éric Davi Nunes <162338194+ericdavinunes@users.noreply.github.com>
2025-06-15 15:40:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// app.js
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json'); // caminho para o seu arquivo Swagger
 
 const transacoesRoutes = require('./routes/transacoes.routes');
 
@@ -11,11 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Swagger
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Rotas da API
 app.use('/api/transacoes', transacoesRoutes);
 
-
-
+// SPA frontend (Angular/React/etc.)
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
